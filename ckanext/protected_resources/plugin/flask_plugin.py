cb7f39f650d92918742e67e00c2496cea14eda3d
Niall McAndrew <niall@mediasuite.co.nz>
2021-08-05 17:49:19 +1200

    Remove debug logging.

--- ckanext/protected_resources/plugin/flask_plugin.py
+++ ckanext/protected_resources/plugin/flask_plugin.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from six import text_type
-from logging import getLogger
-
 import ckan.plugins as p
 import ckanext.protected_resources.views as views
 import ckanext.protected_resources.cli as cli
-
-
-log = getLogger(__name__)
 
 
 class Mixin(p.SingletonPlugin):
@@ -17,9 +11,7 @@ class Mixin(p.SingletonPlugin):
 
     # ICLick
     def get_commands(self):
-        commands = cli.get_commands()
-        log.info("Registering commands %s", text_type(commands[0].commands))
-        return commands
+        return cli.get_commands()
 
     # IBlueprint
     def get_blueprint(self):
